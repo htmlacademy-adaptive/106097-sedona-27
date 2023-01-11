71ee9167d93b25137317ff2f1585cb26f2a161a3
Nikolay Stebunov <ponochevnii@mail.ru>
2023-01-11 22:03:55 +0300

    Add styles and script for navigation

---
+++ source/js/app.js
@@ -0,0 +1,8 @@
+let nav = document.querySelector('.main-navigation');
+let navToggle = document.querySelector('.page-header__toggle');
+
+
+navToggle.addEventListener('click', () => {
+  nav.classList.toggle('main-navigation--closed');
+  navToggle.classList.toggle('page-header__toggle--opened');
+});
