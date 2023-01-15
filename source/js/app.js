59350c2f6b8f7fdc038b47d998ef86f2a1f52bdb
Nikolay Stebunov <ponochevnii@mail.ru>
2023-01-15 14:29:21 +0300

    Add nojs styles improvement

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,10 @@
 let nav = document.querySelector('.main-navigation');
 let navToggle = document.querySelector('.page-header__toggle');
+let logo = document.querySelector('.page-header__logo');
 
-navToggle.classList.remove('main-navigation--nojs');
+nav.classList.remove('main-navigation--nojs');
+logo.classList.remove('page-header__logo--nojs');
+navToggle.classList.remove('page-header__toggle--nojs');
 
 navToggle.addEventListener('click', () => {
   nav.classList.toggle('main-navigation--closed');
