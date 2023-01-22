b3705e14862d0982ea727efd469b4bc8a30f225e
Nikolay Stebunov <ponochevnii@mail.ru>
2023-01-22 14:37:59 +0300

    Update header navigation

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,5 @@
 let nav = document.querySelector('.main-navigation');
+let siteList = document.querySelector('.site-list');
 let navToggle = document.querySelector('.page-header__toggle');
 let logo = document.querySelector('.page-header__logo');
 
@@ -9,4 +10,5 @@ navToggle.classList.remove('page-header__toggle--nojs');
 navToggle.addEventListener('click', () => {
   nav.classList.toggle('main-navigation--closed');
   navToggle.classList.toggle('page-header__toggle--opened');
+  siteList.classList.toggle('site-list--opened');
 });
