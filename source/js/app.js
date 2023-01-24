56bfabc988366848a1006d124347f48c27b0980d
Nikolay Stebunov <ponochevnii@mail.ru>
2023-01-24 11:19:32 +0300

    Add map graceful degradation

--- source/js/app.js
+++ source/js/app.js
@@ -2,10 +2,12 @@ let nav = document.querySelector('.main-navigation');
 let siteList = document.querySelector('.site-list');
 let navToggle = document.querySelector('.page-header__toggle');
 let logo = document.querySelector('.page-header__logo');
+let mapImage = document.querySelector('.map__image');
 
 nav.classList.remove('main-navigation--nojs');
 logo.classList.remove('page-header__logo--nojs');
 navToggle.classList.remove('page-header__toggle--nojs');
+mapImage.classList.remove('map__image--nojs');
 
 navToggle.addEventListener('click', () => {
   nav.classList.toggle('main-navigation--closed');
