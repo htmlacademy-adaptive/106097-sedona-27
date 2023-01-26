7c05a75cdab89c1ae63e493afda91d1ebe9222a7
Nikolay Stebunov <ponochevnii@mail.ru>
2023-01-26 13:03:23 +0300

    Fix logo nojs issue

--- source/js/app.js
+++ source/js/app.js
@@ -5,12 +5,14 @@ let logo = document.querySelector('.page-header__logo');
 let mapImage = document.querySelector('.map__image');
 let header = document.querySelector('.page-header');
 let mapInteractive = document.querySelector('.map__interactive');
+let hero = document.querySelector('.hero');
 
 nav.classList.remove('main-navigation--nojs');
 logo.classList.remove('page-header__logo--nojs');
 navToggle.classList.remove('page-header__toggle--nojs');
 siteList.classList.remove('site-list--opened');
 header.classList.remove('page-header--nojs');
+hero.classList.remove('hero--nojs');
 
 if (mapImage) {
   mapImage.classList.remove('map__image--nojs');
